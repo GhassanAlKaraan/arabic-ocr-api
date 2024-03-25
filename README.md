974957346b98215e5e00eae400f1137a5c215dfc
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-03-25 16:37:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Arabic OCR Demo
+
+1- Install the required dependency:
+
+`pip install ArabicOcr`
+
+2- Replace the source image "1.png" with the image you want to analyse.
+
+3- Run the python file:
+
+`python main.py`
+
+**Note:** When you run the python file for the first time, it will download required models for recognition
+
---
+++ main.py
@@ -0,0 +1,21 @@
+# I'm creating a working python script to analyse arabic text from images
+from ArabicOcr import arabicocr
+
+if __name__ == '__main__':
+    image_path = '1.png'
+    out_image = 'out.png'
+    results = arabicocr.arabic_ocr(image_path, out_image)
+    print(results)
+    words = []
+    for i in range(len(results)):
+        word = results[i][1]
+        words.append(word)
+    with open('file.txt', 'w', encoding='utf-8') as myfile:
+        myfile.write(str(words))
+    import cv2
+
+    img = cv2.imread('out.png', cv2.IMREAD_UNCHANGED)
+    cv2.imshow("arabic ocr", img)
+    cv2.waitKey(0)
+else:
+    print('Running code outside main')
