04917028a6a3eff31cf2e35259eb97c7da0c87a4
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-03-25 17:07:17 +0200

    testing

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from ArabicOcr import arabicocr
 
 if __name__ == '__main__':
-    image_path = '1.png'
-    out_image = 'out.png'
+    image_path = '1.jpg'
+    out_image = 'out.jpg'
     results = arabicocr.arabic_ocr(image_path, out_image)
     print(results)
     words = []
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         myfile.write(str(words))
     import cv2
 
-    img = cv2.imread('out.png', cv2.IMREAD_UNCHANGED)
+    img = cv2.imread('out.jpg', cv2.IMREAD_UNCHANGED)
     cv2.imshow("arabic ocr", img)
     cv2.waitKey(0)
 else:
